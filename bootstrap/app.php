7d845b121b47cb957b21618a96895027549d841a
hidayatusb <muh.hidayatullah@unsulbar.ac.id>
2026-08-26 17:36:08 +0800

    update api setting

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,13 +5,19 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
+$routing = [
+    'web' => __DIR__.'/../routes/web.php',
+    'commands' => __DIR__.'/../routes/console.php',
+    'health' => '/up',
+];
+
+// Matikan API: set API_ENABLED=false di .env lalu php artisan config:clear
+if (filter_var(env('API_ENABLED', true), FILTER_VALIDATE_BOOLEAN)) {
+    $routing['api'] = __DIR__.'/../routes/api.php';
+}
+
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
+    ->withRouting(...$routing)
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => \App\Http\Middleware\EnsureUserRole::class,
